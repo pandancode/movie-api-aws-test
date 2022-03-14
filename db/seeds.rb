8f3872037f90a033551dc8844bb3127dafd9cf9b
Yilun Chen <yilun.chen@web.de>
2022-03-14 15:58:46 +0000

    Basic rails stuff is done, configuration comes next

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+movies = Movie.create([
+    { name: 'Star Wars', year: 1977, genre: 'SCI-FI'},
+    { name: 'Lord of the Rings', year: 2001, genre: 'Fantasy' }
+])
